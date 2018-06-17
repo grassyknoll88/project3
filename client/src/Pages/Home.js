880174ae0055cbacc5a19308d2e9b66cb3d1387d
Melinda Hardin <melindawad@gmail.com>
2018-06-17 18:03:43 -0500

    routes set

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -8,6 +8,8 @@ const Home = () => (
      <HeroPic backgroundImage="https://ibb.co/b1wAkJ">
      <h1>PupLife</h1>
      <h2>Does your pup need some new friends? You came to the right place!</h2>
-     </Hero>
- </div>;
-)
+     </HeroPic>
+ </div>
+);
+
+export default Home;
--- client/src/Pages/SignUp.js
+++ client/src/Pages/SignUp.js
@@ -1,1 +1,20 @@
+import React, { Component } from "react";
 import Container from "../components/Container";
+
+class SignUp extends Component {
+	state = {
+    test1: "One",
+    test2: "Two"
+  };
+
+  render() {
+    return (
+			<Container >
+				<p>This is a SIGN UP PAGE test: {this.state.test1}</p>
+			</Container>
+		);
+  }
+}
+
+export default SignUp;
+
