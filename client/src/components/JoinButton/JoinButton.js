32e201c25606507cbcfb59e952c7b9a070833a48
Kiley Adams <Kileyadams92@gmail.com>
2018-06-17 15:29:07 -0500

    working on components and pages

---
+++ client/src/components/JoinButton/JoinButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./JoinButton.css";
+
+const JoinBtn = props => (
+  <button onClick={props.onClick} className={"btn-primary"} />
+);
+
+export default JoinBtn;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Nav = props => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link className="navbar-brand" to="/">
+      PupLife
+    </Link>
+    <div>
+      <ul className="navbar-nav">
+        <li
+          className={
+            window.location.pathname === "/" ||
+            window.location.pathname === "/home"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/profile"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/profile" className="nav-link">
+            Profile
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/searchdash"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/searchdash" className="nav-link">
+            Search
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Nav;
