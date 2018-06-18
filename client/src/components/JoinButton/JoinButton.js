f513f491a26168fde109fd03a6a1a597eca46065
Kiley Adams <Kileyadams92@gmail.com>
2018-06-17 21:28:21 -0500

    background picture rendering, css modifications made

--- client/src/components/JoinButton/JoinButton.js
+++ client/src/components/JoinButton/JoinButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./JoinButton.css";
 
 const JoinButton = props => (
-  <button onClick={props.onClick} className={"btn-primary"}>
+  <button onClick={props.onClick} className={"btn"}>
     JOIN TODAY
   </button>
 );
