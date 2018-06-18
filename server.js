feb36b43216c627fb15c81219caeb5340556e054
Ben Knoll <bknoll66@gmail.com>
2018-06-18 14:32:32 -0500

    image upload code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path")
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -20,6 +20,14 @@ app.use(bodyParser.json());
 // Static directory to be served
 app.use(express.static("public"));
 
+// image s3 uploader config
+app.use(
+  "/s3",
+  require("react-dropzone-s3-uploader/s3router")({
+    bucket: "pup-life"
+  })
+);
+
 // We need to use sessions to keep track of our user's login status
 // app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 // app.use(passport.initialize());
@@ -30,8 +38,8 @@ app.use(express.static("public"));
 // require("./routes/api-routes.js")(app);
 // Starts the server to begin listening
 // =============================================================
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
