91b7b9ee015bea0ea496f7698ed67f1aab0ab612
Kiley Adams <Kileyadams92@gmail.com>
2018-06-17 19:27:53 -0500

    working on image displaying on hero

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,10 @@ import React, { Component } from "react";
 // import DogCards from "./components/DogCards/DogCards";
 import HeroPic from "./components/HeroPic/HeroPic";
 import JoinButton from "./components/JoinButton/JoinButton";
+import picture from "./assets/dogs.jpg";
 
 const App = () => (
-  <HeroPic backgroundImage="https://ibb.co/b1wAkJ">
+  <HeroPic backgroundImage={picture}>
     <h1>PupLife</h1>
     <h2>Does your pup need some new friends? You came to the right place!</h2>
     <JoinButton />
