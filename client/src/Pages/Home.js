060b9602af08ae41b396da5164fd5e7e7aa5840f
Kiley Adams <Kileyadams92@gmail.com>
2018-06-17 20:51:43 -0500

    continued building Home.js page, incorporating InfoBoxes, HeroPic, Container, and JoinButton - minor bugs enountered with router WIP

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Container from "../components/Container";
 import HeroPic from "../components/HeroPic";
-
+import JoinButton from "../components/JoinButton/JoinButton";
+import picture from "../assets/dogs.jpg";
+import Nav from "../components/Nav/Nav";
+import InfoBoxes from "../components/InfoBoxes/InfoBoxes";
 
 const Home = () => (
- <div>
-     <HeroPic backgroundImage="https://ibb.co/b1wAkJ">
-     <h1>PupLife</h1>
-     <h2>Does your pup need some new friends? You came to the right place!</h2>
-     </HeroPic>
- </div>
+  <Container>
+    <div className="row">
+      <Nav />
+      <HeroPic backgroundImage={picture}>
+        <h1>PupLife</h1>
+        <h2>
+          Does your pup need some new friends? You came to the right place!
+        </h2>
+        <JoinButton />
+      </HeroPic>
+      <div className="row">
+        <InfoBoxes />
+      </div>
+    </div>
+  </Container>
 );
 
 export default Home;
