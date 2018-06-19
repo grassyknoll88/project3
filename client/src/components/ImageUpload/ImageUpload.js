81831cec0df90de2bce9d31c43319cea16bb29f9
Ben Knoll <32990938+grassyknoll88@users.noreply.github.com>
2018-06-18 20:48:05 -0500

    Revert "Revert "image upload changes""

--- client/src/components/ImageUpload/ImageUpload.js
+++ client/src/components/ImageUpload/ImageUpload.js
@@ -1,3 +1,6 @@
+
+
+
 // import React, { Component } from "react";
 // import "./ImageUpload.css";
 // import DropzoneS3Uploader from "react-dropzone-s3-uploader";
@@ -7,6 +10,9 @@
 // //     console.log("File uploaded with filename", info.filename);
 // //     console.log("Access it on s3 at", info.fileUrl);
 // //   };
+
+
+
 
 //   render() {
 //     const uploadOptions = {
@@ -25,21 +31,6 @@
 //   }
 // }
 
-// //   render() {
-// //     const uploadOptions = {
-// //       server: "http://localhost:3000",
-// //       signingUrlQueryParams: { uploadType: "avatar" }
-// //     };
-// //     const s3Url = "https://my-bucket.s3.amazonaws.com";
-// //     return (
-// //       <DropzoneS3Uploader
-// //         onFinish={this.handleFinishedUpload}
-// //         s3Url={s3Url}
-// //         maxSize={1024 * 1024 * 5}
-// //         upload={uploadOptions}
-// //       />
-// //     );
-// //   }
-// // }
+// export default ImageUpload;
 
-// // export default ImageUpload;
+
