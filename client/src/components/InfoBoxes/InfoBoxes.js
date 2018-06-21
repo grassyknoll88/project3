c4b9777cf78bc0fe58ccf369d836a70e2f14704b
Kiley Adams <Kileyadams92@gmail.com>
2018-06-21 18:18:06 -0500

    modified containers/rows/columns and centered infoBox content

--- client/src/components/InfoBoxes/InfoBoxes.js
+++ client/src/components/InfoBoxes/InfoBoxes.js
@@ -12,7 +12,7 @@ const InfoBoxes = props => (
 
     <Container>
       <div className="row">
-        <div className="col-6">
+        <div className="col-4">
           <h3 className="info">
             We have dogs. We love dogs. Most of all, we understand the
             importance of finding activities and playmates to make your pup
@@ -20,6 +20,16 @@ const InfoBoxes = props => (
             you've got a lonely pup or two at home, look no farther - you have
             found your happy place!
           </h3>
+        </div>
+        <div className="col-2" />
+        <div className="col-4">
+          <img
+            src="/assets/dogfriends.jpg"
+            alt="dogfriends"
+            className="dogspic"
+            height="600"
+            width="400"
+          />
         </div>
       </div>
     </Container>
