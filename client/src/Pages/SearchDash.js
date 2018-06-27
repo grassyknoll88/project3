06d9bc38208f90a0814af5e2455e33ba77094046
Kiley Adams <Kileyadams92@gmail.com>
2018-06-27 16:29:13 -0500

    added code for componentDidMount, still in progress, commented out for time being

--- client/src/Pages/SearchDash.js
+++ client/src/Pages/SearchDash.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Container from "../components/Container";
 import DogCards from "../components/DogCards/DogCards";
 import SearchForm from "../components/SearchForm/SearchForm";
+import passport from "passport";
+import API from "../utils/API";
 
 class SearchDash extends Component {
   state = {
@@ -9,15 +11,35 @@ class SearchDash extends Component {
     test2: "Two"
   };
 
+  // componentDidMount() {
+  //   API.getDogs(data).then(data => {
+  //     app.post(
+  //       "/signup",
+  //       passport.authenticate("local-signup", {
+  //         successRedirect: "/profile",
+
+  //         failureRedirect: "/signup"
+  //       })
+  //     );
+
+  //     app.post(
+  //       "/login",
+  //       passport.authenticate("local-signin", {
+  //         successRedirect: "/profile",
+  //         failureRedirect: "/"
+  //       })
+  //     );
+  //   });
+  // }
+
   render() {
     return (
       <Container>
         <SearchForm />
-        <DogCards
-        // dogs={this.state.dogs}  example
-        />
+        <DogCards dogs={this.state.dogs} />
       </Container>
     );
   }
 }
+
 export default SearchDash;
