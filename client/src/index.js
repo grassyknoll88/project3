cf290b560f4b32a01ede834a77593c6df72e13a0
Ben Knoll <bknoll66@gmail.com>
2018-06-28 18:27:19 -0500

    image uploader working

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,19 @@ import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 // import Home from "./Pages/Home";
+import ImageUpload from "./components/ImageUpload";
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBwQaN6FFx5yjUBtTnB-DVmkcBHWgjPA88",
+  authDomain: "puplife-9a9c7.firebaseapp.com",
+  databaseURL: "https://puplife-9a9c7.firebaseio.com",
+  projectId: "puplife-9a9c7",
+  storageBucket: "puplife-9a9c7.appspot.com",
+  messagingSenderId: "278913242521"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
