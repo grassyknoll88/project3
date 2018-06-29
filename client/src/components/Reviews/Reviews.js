b8a85a419cbee11fa18bc94a2bab29b5828d991d
Ben Knoll <bknoll66@gmail.com>
2018-06-28 19:43:27 -0500

    reviews box added

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -1,35 +1,93 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import StarRatingComponent from "react-star-rating-component";
+import "./Reviews.css";
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      rating: 1
+const commentData = [
+  {
+    author: "Shawn Spencer",
+    text: "I've heard it both ways"
+  },
+  {
+    author: "Burton Guster",
+    text: "You hear about Pluto? That's messed up"
+  }
+];
+const CommentBox = React.createClass({
+  getInitialState: function() {
+    return {
+      data: commentData
     };
-  }
-
-  onStarClick(nextValue, prevValue, name) {
-    this.setState({ rating: nextValue });
-  }
-
-  render() {
-    const { rating } = this.state;
-
+  },
+  handleCommentSubmit: function(comment) {
+    this.props.data.push(comment);
+    const comments = this.state.data;
+    const newComments = comments.concat([comment]);
+    this.setState({ data: newComments });
+  },
+  render: function() {
     return (
-      <div>
-        <h2>Rating from state: {rating}</h2>
-        <StarRatingComponent
-          name="rate1"
-          starCount={5}
-          value={rating}
-          onStarClick={this.onStarClick.bind(this)}
+      <div className="comment-box">
+        <CommentForm
+          data={this.props.data}
+          onCommentSubmit={this.handleCommentSubmit}
         />
+        <CommentList data={this.props.data} />
       </div>
     );
   }
-}
+});
+const CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="comment-list">
+        {this.props.data.map(function(c) {
+          return <Comment author={c.author} text={c.text} />;
+        })}
+      </div>
+    );
+  }
+});
+const CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    const authorVal = e.target[0].value.trim();
+    const textVal = e.target[1].value.trim();
+    if (!textVal || !authorVal) {
+      return;
+    }
+    this.props.onCommentSubmit({ author: authorVal, text: textVal });
+    e.target[0].value = "";
+    e.target[1].value = "";
+    return;
+  },
+  render: function() {
+    return (
+      <form className="comment-form form-group" onSubmit={this.handleSubmit}>
+        <div className="input-group">
+          <span className="input-group-addon">Name</span>
+          <input type="text" placeholder="Your name" className="form-control" />
+        </div>
+        <div className="input-group">
+          <span className="input-group-addon">Comment</span>
+          <input
+            type="text"
+            placeholder="Say something..."
+            className="form-control"
+          />
+        </div>
+        <input type="submit" value="Post" className="btn btn-primary" />
+      </form>
+    );
+  }
+});
+const Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="author">{this.props.author}</h2>
+        {this.props.text}
+      </div>
+    );
+  }
+});
 
-ReactDOM.render(<App />, document.getElementById("app"));
+export default CommentBox;
