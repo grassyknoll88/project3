d0ef3002888c7de911160401f31be58b3bb23c7d
Kiley Adams <Kileyadams92@gmail.com>
2018-06-29 16:25:18 -0500

    working on searchform dropdown

--- client/src/Pages/SearchDash.js
+++ client/src/Pages/SearchDash.js
@@ -2,19 +2,46 @@ import React, { Component } from "react";
 import Container from "../components/Container";
 import DogCards from "../components/DogCards/DogCards";
 import SearchForm from "../components/SearchForm/SearchForm";
-
+import Select from "react-select";
 class SearchDash extends Component {
   state = {
     test1: "One",
-    test2: "Two"
+    test2: "Two",
+    selectedOption: ""
   };
 
   componentDidMount() {}
 
+  handleChange = selectedOption => {
+    this.setState({ selectedOption });
+    // selectedOption can be null when the `x` (close) button is clicked
+    if (selectedOption) {
+      console.log(`Selected: ${selectedOption.label}`);
+    }
+  };
+
+  handleSelect = value => {
+    console.log(value);
+  };
+
   render() {
     return (
       <Container>
-        <SearchForm />
+        {/* <SearchForm
+          state={this.state}
+          handleChange={this.handleChange}
+          handleSelect={this.handleSelect}
+        /> */}
+        <Select
+          name="form-field-name"
+          value={this.state.selectedOption}
+          onChange={this.handleChange}
+          // selectValue={props.}
+          options={[
+            { value: "one", label: "One" },
+            { value: "two", label: "Two" }
+          ]}
+        />
         <DogCards dogs={this.state.dogs} />
       </Container>
     );
--- client/src/components/SearchForm/SearchForm.js
+++ client/src/components/SearchForm/SearchForm.js
@@ -1,21 +1,31 @@
 import React from "react";
 import "./SearchForm.css";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
 
-const SearchForm = () => {
+const SearchForm = props => {
+  console.log(props);
   return (
-    <div className="input-group md-form form-sm form-2 pl-0">
-      <input
-        className="form-control my-0 py-1 amber-border"
-        type="text"
-        placeholder="Search"
-        aria-label="Search"
-      />
-      <div className="input-group-append">
-        <span className="input-group-text amber lighten-3" id="basic-text1">
-          <i className="fa fa-search text-grey" aria-hidden="true" />
-        </span>
-      </div>
-    </div>
+    <Select
+      name="form-field-name"
+      value={props.state.selectedOption}
+      onChange={props.handleChange}
+      // selectValue={props.}
+      options={[{ value: "one", label: "One" }, { value: "two", label: "Two" }]}
+    />
+    // <div className="input-group md-form form-sm form-2 pl-0">
+    //   <input
+    //     className="form-control my-0 py-1 amber-border"
+    //     type="text"
+    //     placeholder="Search"
+    //     aria-label="Search"
+    //   />
+    //   <div className="input-group-append">
+    //     <span className="input-group-text amber lighten-3" id="basic-text1">
+    //       <i className="fa fa-search text-grey" aria-hidden="true" />
+    //     </span>
+    //   </div>
+    // </div>
   );
 };
 
