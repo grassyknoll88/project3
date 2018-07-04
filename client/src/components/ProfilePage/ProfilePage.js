d0869d7315f6991a0cd7c7ddaf053b9da9283d0a
Ben Knoll <bknoll66@gmail.com>
2018-07-03 19:58:50 -0500

    add messaging button on profile

--- client/src/components/ProfilePage/ProfilePage.js
+++ client/src/components/ProfilePage/ProfilePage.js
@@ -23,6 +23,7 @@ class ProfilePage extends Component {
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </CardText>
+          <Button />
         </CardBody>
       </Card>
     );
