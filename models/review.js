de76cf6a7eab2987b5d38c3fa61757ac91bf6387
Ben Knoll <bknoll66@gmail.com>
2018-07-08 11:13:29 -0500

    added messaging models

--- models/review.js
+++ models/review.js
@@ -1,45 +1,41 @@
 module.exports = function(Sequelize, DataTypes) {
-  
-	var Review = Sequelize.define('Review', {
-		id: { 
-			autoIncrement: true, 
-			primaryKey: true, 
-			type: DataTypes.INTEGER
-        },
-        // user_id: {
-        //     type: DataTypes.INTEGER,
-        //     notEmpty: true
-        //user_id logged in pull from passport
-        
-        // },
-		// reviewer_id: { 
-		// 	type: DataTypes.STRING,
-		// 	notEmpty: true
-        // },
-        // reviewed_id: { 
-		// 	type: DataTypes.INTEGER,
-		// 	notEmpty: true
-		// },
-        review: {
-            type: DataTypes.TEXT,
-            validate: {
-                len: [1, 300]
-            }
-        },
+  var Review = Sequelize.define("Review", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    // user_id: {
+    //     type: DataTypes.INTEGER,
+    //     notEmpty: true
+    //user_id logged in pull from passport
 
-		dog_id: {
-            type: DataTypes.INTEGER
-        },
+    // },
+    // reviewer_id: {
+    // 	type: DataTypes.STRING,
+    // 	notEmpty: true
+    // },
+    // reviewed_id: {
+    // 	type: DataTypes.INTEGER,
+    // 	notEmpty: true
+    // },
+    review: {
+      type: DataTypes.TEXT,
+      validate: {
+        len: [1, 300]
+      }
+    },
 
-
+    dog_id: {
+      type: DataTypes.INTEGER
+    }
+  });
+  Review.associate = function(models) {
+    models.Review.belongsTo(models.Dog, {
+      foriengKey: {
+        allowNull: false
+      }
     });
-    Review.associate = function(models) {
-        models.Review.belongsTo(models.Dog, {
-            foriengKey: {
-                allowNull: false
-            }
-        });
-    };
-	return Review;
-
-}; 
+  };
+  return Review;
+};
