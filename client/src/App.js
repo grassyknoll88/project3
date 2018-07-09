577bc4224d2df4149e552405d3b4f5876339a984
Melinda Hardin <melindawad@gmail.com>
2018-07-09 18:32:21 -0500

    not using

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,49 @@ import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Switch,
-  Redirect
+  Switch
 } from "react-router-dom";
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import { Provider } from "react-redux";
+import store from './store';
+
 import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
 import SignUp from "./Pages/SignUp";
-
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import FooterPage from "./components/Footer/Footer";
 import SearchForm from "./components/SearchForm/SearchForm";
 
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Provider store = { store }>
+      <Router> 
         <div>
           <Nav />
           <Switch>
@@ -25,13 +52,11 @@ class App extends Component {
             <Route exact path="/profile/:id" component={Profile} />
             <Route exact path="/search" component={SearchForm} />
             <Route exact path="/signup" component={SignUp} />
-
-            {/* <Redirect from= "/signup" to="/profile" />
-            <Redirect from= "/" to="/profile" /> */}
           </Switch>
           <FooterPage />
         </div>
       </Router>
+      </Provider>
     );
   }
 }
